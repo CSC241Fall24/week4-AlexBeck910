63b758bf89d9fcbacc73704bc598783710056413
AlexBeck910 <123428360+AlexBeck910@users.noreply.github.com>
2024-09-19 21:35:30 -0400

    Problem1 first attempt

--- src/main/java/Problem1.java
+++ src/main/java/Problem1.java
@@ -1,9 +1,20 @@
 // Problem1.java
 public class Problem1 {
     public static ListNode concatenate(ListNode l1, ListNode l2) {
-        // TODO: Implement the concatenate method
-        // This method should concatenate l2 to l1 and return l1
-        // Remember to create new nodes instead of directly linking l1 to l2
-        return null;
+        ListNode result = new ListNode(l1.val);
+        ListNode current_src = l1;
+        ListNode current_dest = result;
+        while(current_src.next != null) {
+            current_dest.next = new ListNode(current_src.val);
+            current_src = current_src.next;
+            current_dest = current_dest.next;
+        }
+        current_src = l2;
+        while(current_src.next != null) {
+            current_dest.next = new ListNode(current_src.val);
+            current_src = current_src.next;
+            current_dest = current_dest.next;
+        }
+        return result;
     }
 }
