d36d5f943daf9a0c8cf59d9ee0787ceaa9d181b8
AlexBeck910 <123428360+AlexBeck910@users.noreply.github.com>
2024-09-21 20:02:27 -0400

    Update Problem1.java

--- src/main/java/Problem1.java
+++ src/main/java/Problem1.java
@@ -1,20 +1,26 @@
-// Problem1.java
 public class Problem1 {
     public static ListNode concatenate(ListNode l1, ListNode l2) {
+        if (l1 == null) return l2;
+        if (l2 == null) return l1;
+
         ListNode result = new ListNode(l1.val);
         ListNode current_src = l1;
         ListNode current_dest = result;
-        while(current_src.next != null) {
-            current_dest.next = new ListNode(current_src.val);
+
+        while (current_src.next != null) {
             current_src = current_src.next;
+            current_dest.next = new ListNode(current_src.val);
             current_dest = current_dest.next;
         }
+
+        
         current_src = l2;
-        while(current_src.next != null) {
+        while (current_src != null) {
             current_dest.next = new ListNode(current_src.val);
-            current_src = current_src.next;
             current_dest = current_dest.next;
+            current_src = current_src.next;
         }
+
         return result;
     }
 }
